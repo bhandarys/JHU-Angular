aad5132f35695f65f87689826faa1f5eead3aea2
bhandarys <bhandarys@yahoo.com>
2017-02-22 11:59:06 +0530

    Final Submission

--- mod2/app.js
+++ mod2/app.js
@@ -1,68 +1,61 @@
 (function (){
 'use strict';
 
-var shoppingList1 = [
-  {
-    name: "Rolls",
-    quantity: "2"
-  },
-  {
-    name: "Donuts",
-    quantity: "200"
-  },
-  {
-    name: "Cookies",
-    quantity: "300"
-  },
-  {
-    name: "Chocolate",
-    quantity: "5"
-  }
-];
-var shoppingList2 = [];
-
 angular.module("ShoppingListCheckOff", [])
   .controller("ToBuyController", ToBuyController)
   .controller("AlreadyBoughtController", AlreadyBoughtController)
   .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  //ToBuyController.$inject = ['ShoppingListCheckOffService'];
-  function ToBuyController() { //, ShoppingListCheckOffService){
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService){
     var buy = this;
-    buy.ToBuyList = shoppingList1;
+    buy.ToBuyList = ShoppingListCheckOffService.shoppingList1;
 
     buy.onBuy = function(index){
-      onBuy(index);
+      ShoppingListCheckOffService.onBuy(index);
     }
   }
 
-  //AlreadyBoughtController.$inject = ["$scope"];
-  function AlreadyBoughtController(){
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService){
     var bought = this
-    bought.BoughtList = shoppingList2;
+    bought.BoughtList = ShoppingListCheckOffService.shoppingList2;
   }
 
-  function ShoppingListCheckOffService(index){
+  function ShoppingListCheckOffService(){
     var service = this;
+    // Data
+    var shoppingList1 = [
+      {
+        name: "Rolls",
+        quantity: "2"
+      },
+      {
+        name: "Donuts",
+        quantity: "200"
+      },
+      {
+        name: "Cookies",
+        quantity: "300"
+      },
+      {
+        name: "Chocolate",
+        quantity: "5"
+      }
+    ];
+    var shoppingList2 = [];
+    // Till here
+
+    service.shoppingList1 = shoppingList1;
+    service.shoppingList2 = shoppingList2;
+
     service.onBuy = function(index) {
-    function onBuy(index) {
       shoppingList2.push(  {
           name: shoppingList1[index].name,
           quantity: shoppingList1[index].quantity
         }
       );
       shoppingList1.splice(index, 1);
-    }
     };
   }
-
-  function onBuy(index) {
-    shoppingList2.push(  {
-        name: shoppingList1[index].name,
-        quantity: shoppingList1[index].quantity
-      }
-    );
-    shoppingList1.splice(index, 1);
-  };
-
 })();
