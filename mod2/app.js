a44754abc1bd0709a0ca1227a4cafe9ed2f4b0c3
bhandarys <bhandarys@yahoo.com>
2017-02-22 11:33:31 +0530

    Working code, service still not working

--- mod2/app.js
+++ mod2/app.js
@@ -24,35 +24,45 @@ var shoppingList2 = [];
 angular.module("ShoppingListCheckOff", [])
   .controller("ToBuyController", ToBuyController)
   .controller("AlreadyBoughtController", AlreadyBoughtController)
-  .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  ToBuyController.$inject = ["$scope"];
-  function ToBuyController($scope){
-    $scope.ToBuyList = shoppingList1;
+  //ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController() { //, ShoppingListCheckOffService){
+    var buy = this;
+    buy.ToBuyList = shoppingList1;
 
-    $scope.onBuy = function(index) {
+    buy.onBuy = function(index){
+      onBuy(index);
+    }
+  }
+
+  //AlreadyBoughtController.$inject = ["$scope"];
+  function AlreadyBoughtController(){
+    var bought = this
+    bought.BoughtList = shoppingList2;
+  }
+
+  function ShoppingListCheckOffService(index){
+    var service = this;
+    service.onBuy = function(index) {
+    function onBuy(index) {
       shoppingList2.push(  {
           name: shoppingList1[index].name,
           quantity: shoppingList1[index].quantity
         }
       );
       shoppingList1.splice(index, 1);
+    }
     };
   }
 
-  AlreadyBoughtController.$inject = ["$scope"];
-  function AlreadyBoughtController($scope){
-    $scope.BoughtList = shoppingList2;
-  }
-  function ShoppingListCheckOffService(index){
-    // $scope.onBuy = function(index) {
-    //   shoppingList2.push(  {
-    //       name: shoppingList1[index].name,
-    //       quantity: shoppingList1[index].quantity
-    //     }
-    //   );
-    //   shoppingList1.splice(index, 1);
-    // };
-  }
+  function onBuy(index) {
+    shoppingList2.push(  {
+        name: shoppingList1[index].name,
+        quantity: shoppingList1[index].quantity
+      }
+    );
+    shoppingList1.splice(index, 1);
+  };
 
 })();
